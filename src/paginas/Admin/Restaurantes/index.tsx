3be641468a524e0ab970ba7eb3235d2ed025edf9
Samilly <samillynunes19@gmail.com>
2025-04-15 11:12:32 -0300

    feat: fetching restaurants inside Admin page

--- src/paginas/Admin/Restaurantes/index.tsx
+++ src/paginas/Admin/Restaurantes/index.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import axios from "axios";
 
 const RestaurantsAdmin = () => {
     const [restaurants, setRestaurants] = useState<IRestaurante[]>([]);
 
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(response => setRestaurants(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
     return (
-        <TableContainer  component={Paper} >
+        <TableContainer component={Paper} >
             <Table>
                 <TableHead>
                     <TableRow>
@@ -16,11 +23,14 @@ const RestaurantsAdmin = () => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>
-                            Nome aqui
-                        </TableCell>
-                    </TableRow>
+                    {restaurants.map(r => (
+                        <TableRow key={r.id}>
+                            <TableCell>
+                                {r.nome}
+                            </TableCell>
+                        </TableRow>
+
+                    ))}
                 </TableBody>
             </Table>
         </TableContainer>

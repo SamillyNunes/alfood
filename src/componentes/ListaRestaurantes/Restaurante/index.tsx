aee6f9fcb277e0d28fd62b0d56586af2419f6b43
Samilly <samillynunes19@gmail.com>
2025-04-14 21:19:55 -0300

    feat: fetching dishes for restaurant

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import IPrato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
@@ -8,12 +11,19 @@ interface RestauranteProps {
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
 
+  const [dishes, setDishes] = useState<IPrato[]>([]);
+
+  useEffect(()=>{
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(response => setDishes(response.data));
+  },[restaurante.id]);
+
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {dishes?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
